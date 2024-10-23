9bbee138148af5653b17ee59e6e4e5c19699257f
mrDoodleBop <mhcates42@tntech.edu>
2024-10-23 15:09:39 -0500

    Fixed some bugs in the Student class

--- Student.h
+++ Student.h
@@ -5,26 +5,35 @@
     Purpose : Header file for the Student class
 */
 
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include "StudySet.h"
+#include <string>
+
+using namespace std;
+
 // Forward declaration of StudySet class (since StudySet will be included later)
 //class StudySet;
 
+template <typename T>
 class Student {
 private:
     string name;                 // Student's name
-    list<StudySet> studySets;    // Pointer to a list of StudySet objects
+    list<StudySet<T>> studySets;    // Pointer to a list of StudySet objects
 
 public:
     // Constructor to initialize student name
-    Student(string studentName);
+    Student(T studentName);
 
     // Accessor to get the student's name
-    string getName() const;
+    string getName();
 
     // Method to add a StudySet to the student's list of sets
     void addStudySet(StudySet* set);
 
     // Method to print student information and all study sets
-    void printStudentInfo() const;
+    void printStudentInfo();
 
 };
 
