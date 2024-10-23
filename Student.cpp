001d5910d3e1be8f49e19b7f11616013832aa028
macky-mack <mrraleigh42@tntech.edu>
2024-10-23 14:45:54 -0500

    Wrote student class

--- Student.cpp
+++ Student.cpp
@@ -4,3 +4,33 @@
     Date : 10/10/2024
     Purpose : script file for the Student class
 */
+
+#include "Student.h"
+#include "StudySet.h"
+
+Student::Student(string studentName) : name(studentName) {}
+
+string Student::getName() const {
+    return name;
+}
+
+void Student::addStudySet(StudySet* set) {
+    studySets.push_back(set);
+}
+
+void Student::printStudentInfo() const {
+    cout << "Student Name: " << name << endl;
+    cout << "Study Sets:" << endl;
+    for (StudySet* set : studySets) {
+        cout << "- " << set->getSetName() << endl;
+    }
+}
+
+StudySet* Student::getStudySet(const string& setName) {
+    for (StudySet* set : studySets) {
+        if (set->getSetName() == setName) {
+            return set;
+        }
+    }
+    return nullptr;
+}
--- Student.h
+++ Student.h
@@ -4,3 +4,28 @@
     Date : 10/10/2024
     Purpose : Header file for the Student class
 */
+
+// Forward declaration of StudySet class (since StudySet will be included later)
+//class StudySet;
+
+class Student {
+private:
+    string name;                 // Student's name
+    list<StudySet> studySets;    // Pointer to a list of StudySet objects
+
+public:
+    // Constructor to initialize student name
+    Student(string studentName);
+
+    // Accessor to get the student's name
+    string getName() const;
+
+    // Method to add a StudySet to the student's list of sets
+    void addStudySet(StudySet* set);
+
+    // Method to print student information and all study sets
+    void printStudentInfo() const;
+
+};
+
+#endif // STUDENT_H
