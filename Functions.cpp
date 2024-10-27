440b7c8efbcbe563a4640189fed59d79664a5c13
macky-mack <mrraleigh42@tntech.edu>
2024-10-27 16:09:53 -0500

    fixed some minor mistakes

--- Functions.cpp
+++ Functions.cpp
@@ -35,7 +35,8 @@ using namespace std;
 */
 
 
-void editASet(Student* student) {
+void editASet(Student* student) 
+{
     std::string setName, term, newTermOrDefinition;
     int choice;
 
@@ -45,7 +46,8 @@ void editASet(Student* student) {
 
     // Step 2: Search for the study set by name
     StudySet* set = student->findStudySetByName(setName);
-    while (set == nullptr) {
+    while (set == nullptr) 
+    {
         std::cout << "Study set not found. Please enter a valid study set name: ";
         std::getline(std::cin, setName);
         set = student->findStudySetByName(setName);
@@ -56,40 +58,51 @@ void editASet(Student* student) {
     std::cin >> choice;
     std::cin.ignore(); // Ignore remaining newline in input buffer
 
-    if (choice == 1) {
+    if (choice == 1) 
+    {
         // Step 4: Edit a term
         std::cout << "Enter the term you would like to edit: ";
         std::getline(std::cin, term);
 
         // Check if term exists
-        if (set->hasTerm(term)) {
+        if (set->hasTerm(term)) 
+        {
             std::cout << "Enter the new term: ";
             std::getline(std::cin, newTermOrDefinition);
             set->updateTerm(term, newTermOrDefinition);
             std::cout << "The term has been successfully updated.\n";
-        } else {
+        } 
+        else 
+        {
             std::cout << "Term not found in this set.\n";
         }
-    } else if (choice == 2) {
+    } 
+    else if (choice == 2) 
+    {
         // Step 5: Edit a definition
         std::cout << "Enter the term whose definition you want to edit: ";
         std::getline(std::cin, term);
 
         // Check if term exists
-        if (set->hasTerm(term)) {
+        if (set->hasTerm(term)) 
+        {
             std::cout << "Enter the new definition: ";
             std::getline(std::cin, newTermOrDefinition);
             set->updateDefinition(term, newTermOrDefinition);
             std::cout << "The definition has been successfully updated.\n";
-        } else {
+        } 
+        else 
+        {
             std::cout << "Term not found in this set.\n";
         }
-    } else {
+    } 
+    else 
+    {
         std::cout << "Invalid choice. Please enter 1 for term or 2 for definition.\n";
     }
 }
 /*
-}
+
 
 
     MACKENZIE !
@@ -114,7 +127,8 @@ void editASet(Student* student) {
 */
 
 
-void quizASet(Student* student) {
+void quizASet(Student* student) 
+{
     std::string setName;
     int numQuestions;
     char tryAgain;
@@ -125,13 +139,15 @@ void quizASet(Student* student) {
 
     // Step 2: Search for the study set by name
     StudySet* set = student->findStudySetByName(setName);
-    while (set == nullptr) {
+    while (set == nullptr) 
+    {
         std::cout << "Study set not found. Please enter a valid study set name: ";
         std::getline(std::cin, setName);
         set = student->findStudySetByName(setName);
     }
 
-    do {
+    do 
+    {
         // Step 3: Ask for the number of quiz questions
         std::cout << "How many questions would you like the quiz to be? ";
         std::cin >> numQuestions;
@@ -141,7 +157,8 @@ void quizASet(Student* student) {
         int setSize = set->getNumberOfTerms(); // Assuming this method returns total term/definition pairs
 
         // Step 4: Quiz the user
-        for (int i = 0; i < numQuestions; ++i) {
+        for (int i = 0; i < numQuestions; ++i) 
+        {
             int index = i % setSize; // Loop back if numQuestions > setSize
             std::string definition = set->getDefinitionAt(index);
             std::string correctTerm = set->getTermAt(index);
@@ -151,10 +168,13 @@ void quizASet(Student* student) {
             std::getline(std::cin, userAnswer);
 
             // Step 4c: Check answer
-            if (userAnswer == correctTerm) {
+            if (userAnswer == correctTerm) 
+            {
                 std::cout << "Correct!\n";
                 score++;
-            } else {
+            } 
+            else
+            {
                 std::cout << "Incorrect. The correct answer was: " << correctTerm << "\n";
             }
         }
@@ -168,9 +188,8 @@ void quizASet(Student* student) {
         std::cin >> tryAgain;
         std::cin.ignore(); // Ignore remaining newline in input buffe
 
-}
-
-*
+    }
+}   
 
 
 void createASet(Student* user){ 
